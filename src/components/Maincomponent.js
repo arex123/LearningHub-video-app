6dc9393a65303a888748e1faf151e6e3b51285d8
arex123 <ad47kumar@gmail.com>
2024-07-09 22:22:12 +0530

    course

--- src/components/Maincomponent.js
+++ src/components/Maincomponent.js
@@ -14,3 +14,17 @@ const MainComponent = () => {
 }
 
 export default MainComponent
+
+/*
+Todo:
+
+1: make tag buttonList working
+2: change google icons to ant.design icons
+3: when course is empty add no course design
+4: inside course list add remove course button
+5: add more in courselist section for changing to a filter section
+6: add skeleton to maincontainer and courses list and searchlist
+
+for adding or removing or viewing course you need to sign in first 
+
+*/
